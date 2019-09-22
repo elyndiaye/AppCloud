9b53c3ee5a18ff05819c0c9281dcc5f269946446
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-09-21 21:35:29 -0300

    Protocol

---
+++ App/Datasources/ItemSelectionDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ItemSelectionDelegate.swift
+//  App
+//
+//  Created by ely.assumpcao.ndiaye on 21/09/19.
+//  Copyright © 2019 ely.assumpcao.ndiaye. All rights reserved.
+//
+
+import Foundation
+
+protocol ItemSelectionDelegate: class {
+    func didSelect(item: Item)
+}
